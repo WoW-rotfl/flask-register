e6494072cecad4fad0ebd564367a037cf9a05977
Vlad Armizonov <wow-rotfl@yandex.ru>
2021-09-01 19:36:33 +0300

    Add blog rouutes and index template

--- app/__init__.py
+++ app/__init__.py
@@ -35,4 +35,8 @@ def create_app(test_config=None):
     from app import auth
     app.register_blueprint(auth.bp)
 
+    from app import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
